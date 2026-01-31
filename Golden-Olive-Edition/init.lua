532b345012f8dbd8aedbee3cdcd0063b143ff002
ArabianKnightHK <strongholdp30@gmail.com>
2026-01-31 12:39:12 +0330

    Delete Golden-Olive-Edition directory

--- Golden-Olive-Edition/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  enable = function(self, config)
-    modules.files:registerFileSource('ucp/plugins/Golden-Olive-Edition-1.0.0/resources/')
-  end,
-
-  disable = function(self, config)
-
-  end,
-}
