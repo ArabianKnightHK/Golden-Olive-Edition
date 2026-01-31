8f32cc3f6d9a4ccdf47056f20f4b6a75bcb5216b
ArabianKnightHK <strongholdp30@gmail.com>
2026-01-31 12:39:59 +0330

    Add files via upload

---
+++ Golden-Olive-Edition-1.0.0/init.lua
@@ -0,0 +1,9 @@
+return {
+  enable = function(self, config)
+    modules.files:registerFileSource('ucp/plugins/Golden-Olive-Edition-1.0.0/resources/')
+  end,
+
+  disable = function(self, config)
+
+  end,
+}
